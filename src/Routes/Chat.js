1b46b31153495b4aef55ef9ac582c658cf2d4160
Katelyn Shapoval <katelyn.shapoval@gmail.com>
2024-11-14 09:43:05 +0100

    chat basics

--- src/Routes/Chat.js
+++ src/Routes/Chat.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "../css/chat.css";
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 function Chat() {
   return (
     <div className="chat">
       <h1>Chat</h1>
+      <ul className="feed"></ul>
+      <div className="inputContainer">
+        <input id="userInput" placeholder="Enter your prompt" />
+        <div id="submit">
+          <FaCircleArrowUp />
+        </div>
+      </div>
     </div>
   );
 }

3a4d1fa510bf0e074bc30c45d2eeba407eb90d96
Katelyn Shapoval <katelyn.shapoval@gmail.com>
2025-02-10 13:59:09 +0100

    tips changes

--- src/Routes/Tips.js
+++ src/Routes/Tips.js
@@ -25,6 +25,38 @@ function Tips() {
   });
   const [editingTipId, setEditingTipId] = useState(null);
   const [openedComment, setOpenedComment] = useState(null);
+  const professionalTips = [
+    {
+      Name: "Snacks de movimiento",
+      Description:
+        "Que consiste en cada 20/30 mins intentar ponerlos de pie y hacer algún movimiento articular, andar o hacer incluso alguna sentadilla o lo que queramos.",
+      id: 1,
+    },
+    {
+      Name: "Actividad física",
+      Description:
+        "Hay que priorizar la actividad física incluso las semanas de exámenes, para oxigenar y limpiar el cerebro, y mover las estructuras del cuerpo.",
+      id: 2,
+    },
+    {
+      Name: "⁠Higiene postural",
+      Description:
+        "No es que existan posturas malas ni buenas, lo malo son posturas mantenidas, así que cada cierto tiempo cambiar de posición.",
+      id: 3,
+    },
+    {
+      Name: "Ergonomía ",
+      Description:
+        "Ergonomía en el escritorio, en cuanto a altura de sillas, ratón, teclado, pantalla central alineada con nuestro cuerpo.",
+      id: 4,
+    },
+    {
+      Name: "⁠Higiene del sueño",
+      Description:
+        "Muy importante organizar y respetar el descanso, para tener mayor retención de lo que hayamos estudiado, y también para que los tejidos que puedan estar bajo estrés mecánico, que descansen.",
+      id: 5,
+    },
+  ];
 
   // Authentication state
   useEffect(() => {
@@ -130,7 +162,31 @@ function Tips() {
   return (
     <div className="tips">
       <h1>Tips</h1>
+      <div className="tipSection">
+        <h2>Tips by professionals</h2>
+        <div className="tipCardContainer">
+          {professionalTips.map((tip) => (
+            <div key={tip.id} className="tipCard">
+              <div className="tipCardText">
+                <h2>{tip.Name}</h2>
+                <p>{tip.Description}</p>
+              </div>
+              <div className="tipCardButtons">
+                <button
+                  className="iconTip"
+                  onClick={() => setOpenedComment(tip)}
+                >
+                  <MdComment size={20} />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <hr width="95%" color="black" />
       <div className="tipCardContainer">
+        <h2>Tips by users</h2>
         {tips.length === 0 && !uid ? (
           <p>No tips available</p>
         ) : (
